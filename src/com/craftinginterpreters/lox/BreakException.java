26ac4bc3f26252180a3c879b76ba342cda6e4c0f
MisakaK <1098622641@qq.com>
2023-10-21 16:47:03 +0800

    test2

--- src/com/craftinginterpreters/lox/BreakException.java
+++
@@ -1,9 +0,0 @@
-package com.craftinginterpreters.lox;
-
-class BreakException extends RuntimeException{
-  final Token token;
-  BreakException(Token token, String message) {
-   super(message);
-   this.token = token;
-  }
-}
